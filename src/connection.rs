f1f22de128d4c6c31fa3ca75a7362a0341263b13
arisygdc <arisycso@gmail.com>
2024-05-29 01:08:20 +0700

    create server with custom protocol

---
+++ src/connection.rs
@@ -0,0 +1,75 @@
+use std::{sync::Arc, time::Duration};
+use bytes::BytesMut;
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream, sync::RwLock, time};
+use tokio_rustls::server::TlsStream;
+
+use crate::server::Handler;
+
+pub struct Proxy {
+    // ticker: u32,
+    // access_second: u32,
+    // access_total: u64,
+    authenticated: Arc<ActiveConnection>
+}
+
+impl Proxy {
+    pub fn new(active_connection: Arc<ActiveConnection>) -> Self {
+        Self { authenticated: active_connection }
+    }
+
+    // TODO
+    fn authenticate() -> bool {
+        true
+    }
+}
+
+
+impl Handler for Proxy {
+    async fn process_request(&self, mut stream: TlsStream<TcpStream>) {
+        if !Self::authenticate() {
+            let write_all = stream.write_all("wrong username or password".as_bytes()).await;
+            if let Err(_) = write_all {
+                return ;
+            }
+
+            let mut buffer = BytesMut::with_capacity(1024);
+            let timeout_duration = Duration::from_secs(1);
+            match time::timeout(timeout_duration, stream.read(&mut buffer)).await {
+                Ok(Ok(n)) => {
+                    if n == 0 {
+                        println!("Client closed connection");
+                        return;
+                    }
+                    println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
+    
+                    // Push the connection to authenticated
+                    stream.write_all(b"Ok").await.expect("Failed to write response");
+                    self.authenticated.push_connection(stream).await;
+                }
+                Ok(Err(e)) => {
+                    eprintln!("Error reading from stream: {}", e);
+                    return;
+                }
+                Err(_) => {
+                    eprintln!("Read operation timed out");
+                    return;
+                }
+            }
+        }
+    }
+}
+
+pub struct ActiveConnection {
+    conns: RwLock<Vec<TlsStream<TcpStream>>>
+}
+
+impl ActiveConnection {
+    pub fn new() -> Self {
+        Self { conns: RwLock::new(Vec::with_capacity(8)) }
+    }
+
+    async fn push_connection(&self, stream: TlsStream<TcpStream>)  {
+        let mut conn_writer = self.conns.write().await;
+        conn_writer.push(stream);
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,91 @@
+use std::{fs::File, io::{self, BufReader}, path::Path, sync::Arc};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use tokio::{net::{TcpListener, TcpStream, ToSocketAddrs}, task::JoinHandle};
+use tokio_rustls::{rustls::{pki_types::{CertificateDer, PrivateKeyDer}, ServerConfig}, server::TlsStream, TlsAcceptor};
+
+pub struct Server<H: Handler + Send + Sync + 'static> {
+    cert: Option<CertificatePath>,
+    handler: Arc<H>
+}
+
+impl<H: Handler + Send + Sync + 'static> Server<H> {
+    pub fn new(handler: H, cert: Option<CertificatePath>) -> Self {
+        let handler = Arc::new(handler);
+        Self { cert, handler }
+    }
+
+    pub fn bind<A>(
+        self,
+        addr: A
+    ) -> io::Result<JoinHandle<io::Result<()>>> 
+    where
+        A: ToSocketAddrs + Send + Sync + 'static
+    {
+        let cert = self.cert.as_ref()
+            .ok_or(io::Error::new(
+                io::ErrorKind::NotFound, "certificate"
+            ))?;
+
+        let certs = cert.load_certs()?;
+        let key = cert.load_keys()?;
+
+        let tls_config = tokio_rustls::rustls::ServerConfig::builder()
+            .with_no_client_auth()
+            .with_single_cert(certs, key)
+            .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))?;
+
+        let fut = self.bind_secure(addr, tls_config);
+        Ok(tokio::spawn(fut))
+    }
+
+    async fn bind_secure<A>(
+        self,
+        addr: A, 
+        tls_config: ServerConfig
+    ) -> io::Result<()> where A: ToSocketAddrs + Send {
+        let acceptor = TlsAcceptor::from(Arc::new(tls_config));
+        let listener = TcpListener::bind(&addr).await?;
+        
+        loop {
+            let (stream, _peer_addr) = listener.accept().await?;
+            let acceptor = acceptor.clone();
+            
+            let handle: Arc<H> = self.handler.clone();
+            
+            let fut = async move {
+                let secured_stream = acceptor.accept(stream).await.unwrap();
+                handle.process_request(secured_stream).await;
+            };
+
+            tokio::task::spawn(fut);
+        }
+    }
+}
+
+pub struct CertificatePath {
+    cert: String,
+    private_key: String
+}
+
+impl CertificatePath {
+    pub fn new(cert: String, private_key: String) -> Self {
+        CertificatePath { cert, private_key }
+    }
+
+    fn load_certs(&self) -> io::Result<Vec<CertificateDer<'static>>> {
+        let path = Path::new(&self.cert);
+        certs(&mut BufReader::new(File::open(path)?)).collect()
+    }
+
+    fn load_keys(&self) -> io::Result<PrivateKeyDer<'static>> {
+        let path = Path::new(&self.private_key);
+        pkcs8_private_keys(&mut BufReader::new(File::open(path)?))
+            .next()
+            .unwrap()
+            .map(Into::into)
+    }
+}
+
+pub trait Handler: Send + Sync + 'static {
+    fn process_request(&self, stream: TlsStream<TcpStream>) -> impl std::future::Future<Output = ()> + Send;
+}

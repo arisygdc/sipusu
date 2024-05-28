73f244bf3ed23b373272a3998f6b346e9a2327d4
arisygdc <arisycso@gmail.com>
2024-05-29 01:08:36 +0700

    bind

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::sync::Arc;
+
+use connection::{ActiveConnection, Proxy};
+use server::{CertificatePath, Server};
+use tokio::{join, runtime};
+mod server;
+mod connection;
+
 fn main() {
-    println!("Hello, world!");
+    let build_rt = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build();
+
+    let rt = match build_rt {
+        Ok(v) => v,
+        Err(e) => {
+            println!("[runtime] error: {}", e.to_string());
+            panic!()
+        }
+    };
+    
+    rt.block_on(app())
+}
+
+async fn app() {
+    let active_connection = Arc::new(ActiveConnection::new());
+    let handler = Proxy::new(active_connection.clone());
+    let cert = CertificatePath::new(String::from("/var/test_host/cert.pem"), String::from("/var/test_host/key.pem"));
+    let server = Server::new(handler, Some(cert));
+
+    let bind_addr = "127.0.0.1:3306".to_owned();
+    let rthread1 = server.bind(bind_addr.clone());
+    let thread1 = match rthread1 {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{}", e.to_string());
+            panic!()
+        }
+    };
+
+    let _ = join!(thread1);
 }

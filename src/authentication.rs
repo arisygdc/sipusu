3bfa23d3cf41fa7ea16fdb1de9650d9ce8bfa120
arisygdc <arisycso@gmail.com>
2024-05-30 01:44:41 +0700

    read file and search username

--- src/authentication.rs
+++ src/authentication.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)] 
-use std::{cell::RefCell, io};
+use std::{cell::RefCell, io::{self}};
 
 use bytes::{BufMut, BytesMut};
-use tokio::{fs::{File, OpenOptions}, io::{AsyncSeekExt, AsyncWriteExt}};
+use tokio::{fs::{File, OpenOptions}, io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt}};
 
 const END_RECORD: u8 = 0x1E;
 const GROUP_SEPARATOR: u8 = 0x1D;
@@ -20,7 +20,7 @@ impl Authenticator {
 
     #[inline]
     async fn prepare_storage() -> io::Result<File> {
-        let path = "./wow";
+        let path = "./wow3";
         let mut storage_opt = OpenOptions::new();
         let open_option = storage_opt
             .read(true)
@@ -44,15 +44,41 @@ impl Authenticator {
         }
     }
 
-    // async fn get(&self, username: &str) -> AuthData {
-    //     let mut storage = self.storage.borrow_mut();
-    //     let seeker_index = 0;
+    #[allow(unused_variables)]
+    async fn get(&self, username: &str) -> io::Result<Vec<u8>> {
+        let mut storage = self.storage.borrow_mut();
 
-    //     let seek_from = io::SeekFrom::Current(seeker_index);
-    //     let seeker = storage.seek(seek_from).await;
-    //     storage.
-    //     AuthData::new("username".to_owned(), "password".to_owned())
-    // }
+        let mut seek_idx = 1;
+
+        let buffer_cap = 2048;
+        let mut buffer = BytesMut::with_capacity(buffer_cap);
+        // TODO: Timeout
+        loop {
+            let seek_from = io::SeekFrom::Current(seek_idx);
+            storage.seek(seek_from).await?;
+            storage.read_buf(&mut buffer).await?;
+
+            if buffer.len() == 0 {
+                break;
+            }
+
+            let (pwd, last_eor) = get_comparator(username, &mut buffer);
+            if pwd.len() > 0 {
+                return Ok(pwd);
+            }
+
+            if buffer.len() != last_eor {
+                seek_idx = last_eor as i64
+            }
+
+            if buffer.len() < buffer_cap {
+                break;
+            }
+            unsafe { buffer.set_len(0) };
+        }
+        
+        Ok(Vec::new())
+    }
 
     async fn write(&self, buffer: &mut [u8]) -> io::Result<usize> {
         let mut writer = self.storage.borrow_mut();
@@ -61,6 +87,81 @@ impl Authenticator {
         writer.flush().await?;
         Ok(written)
     }
+}
+
+// TODO: build as struct, buffer into iter
+/// vector leng == 0 indicates username not yet found
+/// return (password bytes, last END_RECORD position)
+fn get_comparator(username: &str, buffer: &mut BytesMut) -> (Vec<u8>, usize) {
+    // read value state
+    // state 1: get username value
+    // state 2: skip reading password
+    // state 3: return AuthData
+    let mut val_state: u8 = 0;
+    let mut val_counter = 0;
+
+    let mut i = 0;
+    let mut last_eor = 0;
+    
+    while i < buffer.len() {
+        if val_state == 1 {
+            let idx = i + val_counter;
+            if idx > buffer.len() {
+                break;
+            }
+            
+            let f = &buffer[i..idx];
+            let vv = f.to_vec();
+            val_state += vv.eq(username.as_bytes()) as u8;
+        }
+
+        else if val_state == 2 {
+            let idx = i + val_counter;
+            if idx > buffer.len() {
+                break;
+            } else if idx == buffer.len() {
+                last_eor += 2;
+                break;
+            }
+            val_state = 0;
+        } 
+        
+        else if val_state == 3 {
+            let idx = i + val_counter;
+            if idx > buffer.len() {
+                break;
+            }
+            
+            let f = &buffer[i..idx];
+            return (f.to_vec(), last_eor);
+        } 
+        
+        else {
+            match buffer[i] {
+                END_RECORD => { last_eor = i; },
+                VALUE_SIGN => { val_state = (val_state % 2) + 1; },
+                GROUP_SEPARATOR => {},
+                _ => {
+                    let mut loop_buf = Vec::with_capacity(8);
+                    while i < buffer.len() {
+                        i+=1;
+                        let b = buffer[i];
+                        if b == VALUE_SIGN {
+                            break;
+                        }
+                        loop_buf.push(b);
+                    }
+                    
+                    val_counter = String::from_utf8(loop_buf)
+                        .unwrap()
+                        .parse()
+                        .unwrap();
+                }
+            }
+        }
+        i+=1;
+    }
+    (Vec::new(), last_eor)
 }
 
 impl AuthenticationStore for Authenticator {
@@ -105,26 +206,21 @@ impl AuthData {
         Self::parts(&self.username, &mut counter, buffer);
         Self::parts(&self.password, &mut counter, buffer);
 
-        counter += 1;
-        buffer.put_bytes(END_RECORD, counter);
-
-        counter
+        buffer.put_u8(END_RECORD);
+        counter + 1
     }
 
     
     fn parts(v: &str, counter: &mut usize, buffer: &mut impl BufMut) {
-        *counter += 1;
-        buffer.put_bytes(GROUP_SEPARATOR, *counter);
+        let v_str_leng = format!("{}", v.len());
+        buffer.put(v_str_leng.as_bytes());
         
-        let len_str = format!("{}", v.len());
-        buffer.put(len_str.as_bytes());
-
-        *counter += len_str.len() + 1;
-        buffer.put_bytes(VALUE_SIGN, *counter);
+        *counter += v_str_leng.len() + 1;
+        buffer.put_u8(VALUE_SIGN);
         
         *counter += v.len() + 1;
         buffer.put(v.as_bytes());
-        buffer.put_bytes(GROUP_SEPARATOR, *counter);
+        buffer.put_u8(GROUP_SEPARATOR);
     }
 }
 

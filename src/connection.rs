16cc6240b968e08eba63c5234b1a6591928c6cc8
arisygdc <arisycso@gmail.com>
2024-05-31 20:41:44 +0700

    handshake

--- src/connection.rs
+++ src/connection.rs
@@ -1,4 +1,4 @@
-use std::{io, sync::Arc, time::Duration};
+use std::{io::{self, ErrorKind}, sync::Arc, time::Duration};
 use bytes::BytesMut;
 use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream, sync::RwLock, time};
 use tokio_rustls::server::TlsStream;
@@ -22,33 +22,46 @@ impl Proxy {
     async fn authenticate(auth: &impl AuthenticationStore, auth_data: &AuthData) -> bool {
         auth.authenticate(auth_data).await
     }
+
+    // TODO: it should produce errors
+    async fn read_stream(stream: &mut TlsStream<TcpStream>, buffer: &mut BytesMut, timeout_sec: u8) -> io::Result<()>{
+        let timeout_duration = Duration::from_secs(timeout_sec as u64);
+
+        match time::timeout(timeout_duration, stream.read(buffer)).await {
+            Ok(read_result) => {
+                let read_leng = read_result?;
+                if read_leng == 0 {
+                    println!("Client closed connection");
+                    return Err(io::Error::new(ErrorKind::ConnectionAborted, "Client closed connection"));
+                }
+                return Ok(());
+            }
+            Err(_) => {
+                return Err(io::Error::new(ErrorKind::TimedOut, "Reading stream timeout"));
+            }
+        }
+    }
 }
 
 impl Handler for Proxy {
+    // TODO: connection id
+    // TODO: create handshake object
     async fn process_request(&self, mut stream: TlsStream<TcpStream>) {
         let mut buffer = BytesMut::with_capacity(1024);
 
-        let timeout_duration = Duration::from_secs(1);
-        let auth_data;
-        match time::timeout(timeout_duration, stream.read(&mut buffer)).await {
-            Ok(Ok(n)) => {
-                if n == 0 {
-                    println!("Client closed connection");
-                    return;
-                }
-
-                auth_data = AuthData::decode(&buffer);
-            }
-            Ok(Err(e)) => {
-                eprintln!("Error reading from stream: {}", e);
-                return;
-            }
-            Err(_) => {
-                eprintln!("Read operation timed out");
-                return;
+        if let Err(read) = Self::read_stream(&mut stream, &mut buffer, 1).await {
+            if let Err(e)  = stream.write(read.to_string().as_bytes()).await {
+                eprint!("{}", e.to_string());
+                return ;
             }
         }
-        
+
+
+        if buffer.len() < 30 {
+            return ;
+        }
+
+        let auth_data = AuthData::decode(&buffer);
         
         if Proxy::authenticate(&self.authenticator, &auth_data).await {
             let write_all = stream.write_all("wrong username or password".as_bytes()).await;
@@ -57,27 +70,34 @@ impl Handler for Proxy {
             }
         }
 
-        let mut buffer = BytesMut::with_capacity(1024);
-        let timeout_duration = Duration::from_secs(1);
-        match time::timeout(timeout_duration, stream.read(&mut buffer)).await {
-            Ok(Ok(n)) => {
-                if n == 0 {
-                    println!("Client closed connection");
-                    return;
-                }
-    
-                stream.write_all(b"Ok").await.expect("Failed to write response");
-                self.authenticated.push_connection(stream).await;
-            }
-            Ok(Err(e)) => {
-                eprintln!("Error reading from stream: {}", e);
-                return;
-            }
-            Err(_) => {
-                eprintln!("Read operation timed out");
-                return;
+        if let Err(e) = stream.write(b"[SYNC]").await {
+            eprintln!("[stream]{}", e.to_string());
+            return ;
+        }
+
+        buffer.clear();
+        if let Err(read) = Self::read_stream(&mut stream, &mut buffer, 1).await {
+            if let Err(e)  = stream.write(read.to_string().as_bytes()).await {
+                eprint!("{}", e.to_string());
+                return ;
             }
         }
+
+        if buffer.len() < 5 {
+            return ;
+        }
+
+        let ack = &buffer[0..5];
+        if !ack.eq("[ACK]".as_bytes()) {
+            return ;
+        }
+
+        if let Err(e) = stream.write(b"[OK]").await {
+            eprintln!("[stream]{}", e.to_string());
+            return ;
+        }
+
+        self.authenticated.push_connection(stream).await;
     }
 }
 

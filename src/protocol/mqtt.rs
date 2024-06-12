f46c72badc201ec106488696a68fa9ab1c799aca
arisygdc <arisycso@gmail.com>
2024-06-13 01:28:07 +0700

    enum client packet

--- src/protocol/mqtt.rs
+++ src/protocol/mqtt.rs
@@ -1,9 +1,14 @@
 use bytes::{Buf, BufMut, BytesMut};
 
-// #[derive(Debug, Serialize, Deserialize)]
-pub enum MqttPacket {
-    Connect(ConnectPacket),
+pub enum MqttClientPacket {
     Publish(PublishPacket),
+    Subscribe(SubscribePacket)
+}
+
+pub struct SubscribePacket {
+    pub topic: String,
+    pub id: u8,
+    pub qos: u8
 }
 
 #[derive(Debug)]
@@ -18,29 +23,6 @@ pub struct ConnectPacket {
 pub struct PublishPacket {
     pub topic: String,
     pub payload: Vec<u8>,
-}
-
-impl MqttPacket {
-    pub fn serialize(&self) -> BytesMut {
-        match self {
-            MqttPacket::Connect(packet) => packet.serialize(),
-            MqttPacket::Publish(packet) => packet.serialize(),
-        }
-    }
-
-    pub fn deserialize(bytes: &mut BytesMut) -> Result<Self, &'static str> {
-        if bytes.is_empty() {
-            return Err("No data to deserialize");
-        }
-
-        let packet_type = bytes[0] >> 4;
-
-        match packet_type {
-            1 => Ok(MqttPacket::Connect(ConnectPacket::deserialize(bytes)?)),
-            3 => Ok(MqttPacket::Publish(PublishPacket::deserialize(bytes)?)),
-            _ => Err("Unknown packet type"),
-        }
-    }
 }
 
 impl ConnectPacket {
@@ -112,6 +94,41 @@ impl ConnectPacket {
     }
 }
 
+impl SubscribePacket {
+    pub fn deserialize(buffer: &mut BytesMut) -> Result<Self, &'static str> {
+        if buffer.get_u8() == 0x82 {
+            Self::skip_header(buffer);
+        }
+        Err("invalid subscribe packet")
+    }
+
+    fn skip_header(buffer: &mut BytesMut) -> Result<Self, &'static str> {
+        let _leng = buffer.get_u8();
+        let id = buffer.get_u8();
+
+        let fleng = buffer.get_u16() as usize;
+        if fleng != buffer.len() -1 {
+            return Err("invalid topic length");
+        }
+        let topic = String::from_utf8(buffer.split_to(fleng).to_vec()).unwrap();
+        let qos = buffer.get_u8();
+
+        Ok(Self { topic, id, qos })
+    }
+}
+
+impl MqttClientPacket {
+    pub fn deserialize(buffer: &mut BytesMut) -> Result<Self, &'static str> {
+        let packet = match buffer.get_u8() {
+            0x82 => Self::Subscribe(SubscribePacket::skip_header(buffer)?),
+            0x30 => Self::Publish(PublishPacket::skip_header(buffer)?),
+            _ => return Err("invalid header")
+        };
+
+        Ok(packet)
+    }
+}
+
 impl PublishPacket {
     pub fn serialize(&self) -> BytesMut {
         let mut buf = BytesMut::new();
@@ -132,23 +149,26 @@ impl PublishPacket {
         buf
     }
 
-    pub fn deserialize(bytes: &mut BytesMut) -> Result<Self, &'static str> {
-        if bytes[0] != 0x30 {
+    pub fn deserialize(buffer: &mut BytesMut) -> Result<Self, &'static str> {
+        if buffer[0] != 0x30 {
             return Err("Invalid PUBLISH packet type");
         }
+        Self::skip_header(buffer)
+    }
 
+    fn skip_header(buffer: &mut BytesMut) -> Result<Self, &'static str> {
         // Skip the packet type byte
-        bytes.advance(1);
+        buffer.advance(1);
 
         // Remaining length
-        let _remaining_length = bytes.get_u8() as usize;
+        let _remaining_length = buffer.get_u8() as usize;
 
         // Topic
-        let topic_len = bytes.get_u16() as usize;
-        let topic = String::from_utf8(bytes.split_to(topic_len).to_vec()).map_err(|_| "Invalid topic")?;
+        let topic_len = buffer.get_u16() as usize;
+        let topic = String::from_utf8(buffer.split_to(topic_len).to_vec()).map_err(|_| "Invalid topic")?;
 
         // Payload
-        let mut payload = bytes.split().to_vec();
+        let mut payload = buffer.split().to_vec();
         let mut payload_leng = payload.len();
         for ( i, p ) in payload.iter().enumerate() {
             if 0.eq(p) { 

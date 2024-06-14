6c77932b52cfaa941e2b159a6d832d8aeddd9630
arisygdc <arisycso@gmail.com>
2024-06-14 12:44:09 +0700

    broker

--- src/message_broker/mediator.rs
+++ src/message_broker/mediator.rs
@@ -1,23 +1,20 @@
-use std::{io, net::SocketAddr, sync::Arc, time::Duration};
-use dashmap::DashMap;
-use tokio::{sync::{Mutex, RwLock}, task::JoinHandle, time};
+use std::{net::SocketAddr, sync::Arc, time::Duration};
+use tokio::{task::JoinHandle, time};
 use crate::protocol::mqtt::PublishPacket;
-use super::{client::Client, consumer::Consumer, linked_list::List, producer::Producer};
-
-pub(super) type Clients = Vec<Mutex<Client>>;
+use super::{client::{Client, Clients}, linked_list::List, provider::EventHandler, trie::Trie, Consumer, Event, EventListener, Messanger};
 
 pub struct BrokerMediator {
-    clients: Arc<RwLock<Clients>>,
+    clients: Clients,
     message_queue: Arc<List<PublishPacket>>,
-    subscriber: Arc<DashMap<String, Clients>>
+    router: Arc<Trie<u32>>,
 }
 
 impl BrokerMediator {
     pub fn new() -> Self {
-        let clients = Arc::new(RwLock::new(Vec::new()));
+        let clients = Clients::new();
         let message_queue = Arc::new(List::new());
-        let subscriber: Arc<DashMap<String, Clients>> = Arc::new(DashMap::new());
-        Self{ clients, message_queue, subscriber }
+        let router = Arc::new(Trie::new());
+        Self{ clients, message_queue, router }
     }
 }
 
@@ -25,69 +22,62 @@ impl BrokerMediator {
     pub async fn register(&self, client: Client) {
         let client = client;
         println!("[register] client {:?}", client);
-        let value = Mutex::new(client);
-        let mut wr = self.clients.write().await;
-        wr.push(value);
+        self.clients.insert(client).await
     }
 
     pub async fn wakeup_exists(&self, clid: &str, addr: &SocketAddr) -> Option<u32> {
-        let read = self.clients.read().await;
-        for c in read.iter() {
-            let mut c = c.lock().await;
-            if !c.client_id.eq(clid) {
-                continue;
-            }
-
+        let res = self.clients.find(clid, |c| {
             if c.addr.eq(addr) {
                 c.set_alive(true);
                 return Some(c.conn_num);
             }
-        }
-        
-        None
+                
+            None
+        }).await?;
+        res
     }
 
     pub fn join_handle(&mut self) -> (JoinHandle<()>, JoinHandle<()>) {
-        let producer = Producer::new(
-            self.clients.clone(),
-            self.message_queue.clone()
+        let clients = self.clients.clone();
+        let action = EventHandler::from(
+            self.message_queue.clone(), 
+            self.router.clone()
         );
     
-        let consumer = Consumer::new(
-            self.message_queue.clone(),
-            self.subscriber.clone(),
-        );
-    
-        let t1 = tokio::spawn(event_listener(producer));
-        let t2 = tokio::spawn(observer_message(consumer));
+        let t1 = tokio::task::spawn(event_listener(clients.clone(), action.clone()));
+        let t2 = tokio::task::spawn(observer_message(action.clone(), clients.clone()));
         (t1, t2)
     }
 }
 
-async fn event_listener<P>(producer: P) 
-    where P: MessageProducer + Send + Sync
+async fn event_listener<L, E>(trig: L, event: E) 
+    where 
+        L: EventListener + Send + Sync,
+        E: Event + Send + Sync
 {
-    loop { producer.listen_clients().await; }
-}
-
-async fn observer_message<C>(consumer: C)
-    where C: MessageConsumer + Send + Sync
-{
-    loop {
-        if let Some(packet) = consumer.take() {
-            consumer.broadcast(packet).await.unwrap();
+    loop { 
+        if trig.count_listener().await == 0 {
+            time::sleep(Duration::from_millis(5)).await;
+            continue;
         }
-        time::sleep(Duration::from_millis(10)).await;
+        trig.listen_all(&event).await; 
     }
 }
 
-pub trait MessageProducer {
-    fn listen_clients(&self) -> impl std::future::Future<Output = ()> + Send;
-}
-
-pub trait MessageConsumer {
-    type T;
-    fn take(&self) -> Option<Self::T>;
-    // TODO: maybe list unsent message with client identier
-    fn broadcast(&self, message: Self::T) -> impl std::future::Future<Output = io::Result<()>> + Send;
+async fn observer_message<M, S>(provider: M, forwarder: S)
+    where 
+        M: Messanger + Send + Sync,
+        S: Consumer + Send + Sync
+{
+    loop {
+        if let Some(packet) = provider.dequeue_message() {
+            let to = provider.route(&packet.topic).await;
+            let dst = match to {
+                None => continue,
+                Some(dst) => dst[0]
+            };
+            forwarder.pubish(dst, packet).await.unwrap();
+        }
+        time::sleep(Duration::from_millis(10)).await;
+    }
 }

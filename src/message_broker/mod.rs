03ed48fd67004c29a7a67d45468a58d83a1b8641
arisygdc <arisycso@gmail.com>
2024-07-01 22:34:21 +0700

    change structure

--- src/message_broker/mod.rs
+++ src/message_broker/mod.rs
@@ -1,8 +1,9 @@
 use std::io;
 use client::client::ClientID;
 
-use crate::protocol::{mqtt::PublishPacket, subscribe::{SubAckResult, Subscribe}};
+use crate::protocol::v5::{publish::PublishPacket, subsack::SubAckResult, subscribe::Subscribe};
 
+mod messenger;
 pub mod client;
 pub mod mediator;
 mod provider;
@@ -12,30 +13,29 @@ pub const WILDCARD_SUPPORT: bool = false;
 pub const SUBS_ID_SUPPORT: bool = false;
 pub const SHARED_SUBS_SUPPORT: bool = false;
 
-// producer
-// - listener(event)
+#[derive(Default)]
+pub struct Message {
+    pub publisher: Option<ClientID>,
+    pub packet: PublishPacket
+}
 
-// consumer
-// - take
-// - lookup(producer) 
-
-pub trait Event {
-    fn enqueue_message(&self, msg: PublishPacket);
-    fn subscribe_topics(&self, sub: &[Subscribe], con_id: ClientID) -> impl std::future::Future<Output = Vec<SubAckResult>> + Send;
+pub trait ClientReqHandle {
+    fn enqueue_message(&self, msg: Message);
+    fn subscribe_topics(&self, sub: &[Subscribe], clid: ClientID) -> impl std::future::Future<Output = Vec<SubAckResult>> + Send;
 }
 
 pub trait EventListener {
     fn listen_all<E>(&self, event: &E) -> impl std::future::Future<Output = ()> + Send
-        where E: Event + Send + Sync;
+        where E: ClientReqHandle + Send + Sync;
 
     fn count_listener(&self) -> impl std::future::Future<Output = usize> + Send;
 }
 
-pub trait Consumer {
-    fn pubish(&self, con_id: ClientID, packet: PublishPacket) -> impl std::future::Future<Output = io::Result<()>> + Send;
+pub trait Forwarder {
+    fn pubish(&self, clid: &ClientID, packet: &[u8]) -> impl std::future::Future<Output = io::Result<()>> + Send;
 }
 
 pub trait Messanger {
-    fn dequeue_message(&self) -> Option<PublishPacket>;
+    fn dequeue_message(&self) -> Option<Message>;
     fn route(&self, topic: &str) -> impl std::future::Future<Output =  Option<Vec<ClientID>>> + Send;
 }

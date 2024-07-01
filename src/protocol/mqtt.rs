b268b3e5924414a90f42347337eccfbebb957b9d
arisygdc <arisycso@gmail.com>
2024-07-01 22:35:34 +0700

    ping

--- src/protocol/mqtt.rs
+++ src/protocol/mqtt.rs
@@ -2,10 +2,13 @@ use bytes::BytesMut;
 
 use super::v5::{publish::PublishPacket, subscribe::SubscribePacket};
 
+pub const PING_RES: [u8; 1] = [0xD0];
+
 
 pub enum ClientPacketV5 {
     Publish(PublishPacket),
-    Subscribe(SubscribePacket)
+    Subscribe(SubscribePacket),
+    PingReq
 }
 
 impl ClientPacketV5 {
@@ -14,6 +17,7 @@ impl ClientPacketV5 {
         let pv = match ctrl_packet {
             0x08 => Self::Subscribe(SubscribePacket::decode(buffer)?),
             0x03 => Self::Publish(PublishPacket::decode(buffer)?),
+            0x0C => Self::PingReq,
             _ => return Err(String::from("invalid header"))
         };
         Ok(pv)

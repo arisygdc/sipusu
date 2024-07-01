a65239a5d01cbbc8de983d09322a256fd9fd5347
arisygdc <arisycso@gmail.com>
2024-07-01 16:50:50 +0700

    change client packet

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,28 +1,2 @@
-use bytes::{Buf, BytesMut};
-
 pub mod mqtt;
-pub mod subscribe;
 pub mod v5;
-
-pub struct RemainingLength;
-
-impl RemainingLength {
-    pub fn decode(buffer: &mut BytesMut) -> Result<u32, &'static str> {
-        let mut multiplier = 1;
-        let mut value = 0;
-        
-        loop {
-            let encoded_byte = buffer.get_u8();
-            value += ((encoded_byte & 127) as u32) * multiplier;
-            if (encoded_byte & 128) == 0 {
-                break;
-            }
-    
-            multiplier *= 128;
-            if multiplier > 128 * 128 * 128 {
-                return Err("Malformed Remaining Length");
-            }
-        }
-        Ok(value)
-    }
-}
--- src/protocol/mqtt.rs
+++ src/protocol/mqtt.rs
@@ -1,89 +1,21 @@
-use bytes::{Buf, BufMut, BytesMut};
+use bytes::BytesMut;
 
-use super::{subscribe::SubscribePacket, v5::{publish::PublishPacket as PubV5, subscribe::SubscribePacket as SubV5}};
+use super::v5::{publish::PublishPacket, subscribe::SubscribePacket};
 
-pub enum V5ClientPacket {
-    Publish(PubV5),
-    Subscribe(SubV5)
-}
 
-impl V5ClientPacket {
-    pub fn decode(buffer: &mut BytesMut) -> Result<Self, String> {
-        let ctrl_packet = buffer[0] >> 4;
-        let pv = match ctrl_packet {
-            0x08 => Self::Subscribe(SubV5::decode(buffer)?),
-            0x03 => Self::Publish(PubV5::decode(buffer)?),
-            _ => return Err(String::from("invalid header"))
-        };
-        Ok(pv)
-    }
-}
-
-pub enum MqttClientPacket {
+pub enum ClientPacketV5 {
     Publish(PublishPacket),
     Subscribe(SubscribePacket)
 }
 
-#[derive(Default, Debug)]
-pub struct PublishPacket {
-    pub topic: String,
-    pub payload: Vec<u8>,
-}
-
-impl MqttClientPacket {
-    pub fn deserialize(buffer: &mut BytesMut) -> Result<Self, &'static str> {
-        let packet = match buffer.get_u8() {
-            0x82 => Self::Subscribe(SubscribePacket::skip_header(buffer)?),
-            0x30 => Self::Publish(PublishPacket::skip_header(buffer)?),
-            _ => return Err("invalid header")
+impl ClientPacketV5 {
+    pub fn decode(buffer: &mut BytesMut) -> Result<Self, String> {
+        let ctrl_packet = buffer[0] >> 4;
+        let pv = match ctrl_packet {
+            0x08 => Self::Subscribe(SubscribePacket::decode(buffer)?),
+            0x03 => Self::Publish(PublishPacket::decode(buffer)?),
+            _ => return Err(String::from("invalid header"))
         };
-
-        Ok(packet)
-    }
-}
-
-
-impl PublishPacket {
-    pub fn serialize(&self) -> BytesMut {
-        let mut buf = BytesMut::new();
-        buf.put_u8(0x30); // PUBLISH packet type
-
-        // Remaining length
-        let remaining_length = 2 + self.topic.len() + self.payload.len();
-        buf.put_u8(remaining_length as u8);
-
-        // Topic
-        buf.put_u8((self.topic.len() >> 8) as u8);
-        buf.put_u8(self.topic.len() as u8);
-        buf.put_slice(self.topic.as_bytes());
-
-        // Payload
-        buf.put_slice(&self.payload);
-
-        buf
-    }
-
-    fn skip_header(buffer: &mut BytesMut) -> Result<Self, &'static str> {
-        let _remaining_length = buffer.get_u8() as usize;
-
-        // Topic
-        let topic_len = buffer.get_u16() as usize;
-        let topic = String::from_utf8(buffer.split_to(topic_len).to_vec()).map_err(|_| "Invalid topic")?;
-
-        // Payload
-        let mut payload = buffer.split().to_vec();
-        let mut payload_leng = payload.len();
-        for ( i, p ) in payload.iter().enumerate() {
-            if 0.eq(p) { 
-                payload_leng = i;
-                break;
-            }
-        }
-
-        payload.drain(payload_leng..);
-        Ok(Self {
-            topic,
-            payload,
-        })  
+        Ok(pv)
     }
 }

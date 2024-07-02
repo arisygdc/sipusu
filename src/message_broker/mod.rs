b8f144ae0326d58404a5d3a32c9bff69133b89f2
arisygdc <arisycso@gmail.com>
2024-07-03 00:07:44 +0700

    major change

--- src/message_broker/mod.rs
+++ src/message_broker/mod.rs
@@ -3,10 +3,9 @@ use client::client::ClientID;
 
 use crate::protocol::v5::{publish::PublishPacket, subsack::SubAckResult, subscribe::Subscribe};
 
-mod messenger;
+mod msg_state;
 pub mod client;
 pub mod mediator;
-mod provider;
 
 pub const MAX_QOS: u8 = 2;
 pub const WILDCARD_SUPPORT: bool = false;
@@ -19,23 +18,26 @@ pub struct Message {
     pub packet: PublishPacket
 }
 
-pub trait ClientReqHandle {
-    fn enqueue_message(&self, msg: Message);
-    fn subscribe_topics(&self, sub: &[Subscribe], clid: ClientID) -> impl std::future::Future<Output = Vec<SubAckResult>> + Send;
-}
+pub trait SendStrategy: Forwarder + Send + Sync
+{
+    async fn qos0(&self, subscriber: &ClientID, buffer: &[u8]);
+    async fn qos1(
+        &self, 
+        publisher: &ClientID, 
+        subscriber: &ClientID, 
+        packet_id: u16, 
+        buffer: &[u8]
+    ) -> std::io::Result<()>;
 
-pub trait EventListener {
-    fn listen_all<E>(&self, event: &E) -> impl std::future::Future<Output = ()> + Send
-        where E: ClientReqHandle + Send + Sync;
-
-    fn count_listener(&self) -> impl std::future::Future<Output = usize> + Send;
+    async fn qos2(
+        &self, 
+        publisher: &ClientID, 
+        subscriber: &ClientID, 
+        packet_id: u16, 
+        buffer: &[u8]
+    ) -> std::io::Result<()>;
 }
 
 pub trait Forwarder {
     fn pubish(&self, clid: &ClientID, packet: &[u8]) -> impl std::future::Future<Output = io::Result<()>> + Send;
-}
-
-pub trait Messanger {
-    fn dequeue_message(&self) -> Option<Message>;
-    fn route(&self, topic: &str) -> impl std::future::Future<Output =  Option<Vec<ClientID>>> + Send;
 }

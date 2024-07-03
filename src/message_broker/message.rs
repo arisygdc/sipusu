84f198116b8a2fb5165d4375edd3175a1a97d852
arisygdc <arisycso@gmail.com>
2024-07-03 14:09:49 +0700

    separate message

---
+++ src/message_broker/message.rs
@@ -0,0 +1,31 @@
+use std::sync::Arc;
+
+use crate::{ds::{linked_list::List, GetFromQueue, InsertQueue}, protocol::v5::publish::PublishPacket};
+use super::{cleanup::Cleanup, client::client::ClientID};
+
+#[derive(Default)]
+pub struct Message {
+    pub publisher: Option<ClientID>,
+    pub packet: PublishPacket
+}
+
+pub type MessageQueue = Arc<List<Message>>;
+
+impl<Message: Default> InsertQueue<Message> for Arc<List<Message>>
+{
+    fn enqueue(&self, val: Message) {
+        self.append(val)
+    }
+}
+
+impl<Message: Default> GetFromQueue<Message> for Arc<List<Message>>{
+    fn dequeue(&self) -> Option<Message> {
+        self.take_first()
+    }
+}
+
+impl Cleanup for MessageQueue {
+    async fn clear(self) {
+        println!("clean message")
+    }
+}
--- src/message_broker/mod.rs
+++ src/message_broker/mod.rs
@@ -1,22 +1,16 @@
 use std::io;
 use client::client::ClientID;
 
-use crate::protocol::v5::{publish::PublishPacket, subsack::SubAckResult, subscribe::Subscribe};
-
 mod msg_state;
 pub mod client;
 pub mod mediator;
+pub mod cleanup;
+mod message;
 
 pub const MAX_QOS: u8 = 2;
 pub const WILDCARD_SUPPORT: bool = false;
 pub const SUBS_ID_SUPPORT: bool = false;
 pub const SHARED_SUBS_SUPPORT: bool = false;
-
-#[derive(Default)]
-pub struct Message {
-    pub publisher: Option<ClientID>,
-    pub packet: PublishPacket
-}
 
 pub trait SendStrategy: Forwarder + Send + Sync
 {

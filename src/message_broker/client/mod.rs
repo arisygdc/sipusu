bccbd188f0b000594eb9b2d596fc2cec8396411b
arisygdc <arisycso@gmail.com>
2024-07-06 16:37:23 +0700

    get expiration time

--- src/message_broker/client/mod.rs
+++ src/message_broker/client/mod.rs
@@ -19,4 +19,5 @@ pub trait SessionController {
     /// generate error when old duration less than `t`
     fn keep_alive(&mut self, t: u64) -> Result<u64, String>;
     fn is_expired(&self, t: u64) -> bool;
+    fn expiration_time(&self) -> u64;
 }
